97545f4174f03a25cc38a93e124409600d36ae70
Frederik Goetz <frederik.goetz@gmail.com>
2018-02-26 20:36:20 +0100

    clean up

--- app/src/main/java/de/gfred/app/MainApplication.kt
+++ app/src/main/java/de/gfred/app/MainApplication.kt
@@ -1,23 +1,11 @@
 package de.gfred.app
 
-import android.app.Activity
-import android.app.Application
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
 import de.gfred.app.di.DaggerAppComponent
-import javax.inject.Inject
 
 
 
 class MainApplication : DaggerApplication() {
-
-
-    override fun onCreate() {
-        super.onCreate()
-    }
-
-
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> = DaggerAppComponent.builder().create(this)
 }
--- features/featureTwo/src/main/java/de/gfred/feature/two/FeatureTwoActivity.kt
+++ features/featureTwo/src/main/java/de/gfred/feature/two/FeatureTwoActivity.kt
@@ -1,18 +1,9 @@
 package de.gfred.feature.two
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class FeatureTwoActivity : AppCompatActivity() {
-
-    companion object {
-        fun start(context : Context) {
-            context.startActivity(Intent(context, FeatureTwoActivity::class.java))
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actvitiy_feature_two)
